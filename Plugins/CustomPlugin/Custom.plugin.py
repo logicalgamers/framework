4933d508a615e74d2aa80038e1b76cec433c4ef1
Matt Smith <matt.joseph.smith@gmail.com>
2012-04-28 14:14:12 -0400

    ... Git sometimes makes me angry.. It added these files, shouldn't have. Moved HTTP class to appropriate file. Updated .gitignore.

--- Plugins/CustomPlugin/Custom.plugin.py
+++ Plugins/CustomPlugin/Custom.plugin.py
@@ -15,23 +15,3 @@ class Custom():
 		else:
 			PostData = None
 		print HTTP.Post(URL, PostData)
-
-import urllib
-import urllib2
-import cookielib
-class HTTPWrapper():
-	def __init__(self):
-		self.cookiejar = cookielib.CookieJar()
-		self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookiejar))
-
-	def Get(self, url):
-		return self._req(url)
-
-	def Post(self, url, data):
-		return self._req(url, data)
-
-	def _req(self, url, data=None):
-		if(data != None):
-			if(not isinstance(data, str)):
-				data = ""
-		return self.opener.open(url, data).read()
