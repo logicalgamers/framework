2b47e9ce4c067782cf3a4254fa4f22b460777461
MattSmith <matt.joseph.smith@gmail.com>
2012-05-24 20:15:38 -0400

    No need for the loop anymore, was just testing multi threading.

--- Main.py
+++ Main.py
@@ -3,8 +3,6 @@ from includes import PluginFramework
 class Main:
     def __init__(self):
         self.plugins = PluginFramework.PluginFramework()
-        while(True):
-            pass
 
 if(__name__ == "__main__"):
     print("Starting up..")
